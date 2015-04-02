1123169f2b6565f7fda68e33ed43493c61b266de
Chad Furman <chad@fuzzproductions.com>
2015-04-02 00:04:06 -0400

    comment list playground with simple tag

---
+++ comment-list/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<!-- 1. Load platform support before any code that touches the DOM. -->
+	<script src="bower_components/webcomponentsjs/webcomponents.min.js"></script>
+
+	<!-- 2. Load the component using an HTML Import -->
+	<link rel="import" href="elements/simple-tag.html">
+	<link rel="import" href="elements/comment-list.html">
+</head>
+<body>
+	<simple-tag></simple-tag>
+	<comment-list></comment-list>
+</body>
+</html>
